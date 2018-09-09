cee6fe229c03f98a4a6d97ef381d97e4ceb72239
Nathan Pabst <nathan.pabst@gmail.com>
2018-09-09 12:46:27 -0500

    odd numbers removed from list

--- RandomSquared/Program.cs
+++ RandomSquared/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace RandomSquared
 {
@@ -28,6 +29,17 @@ namespace RandomSquared
             Console.WriteLine("now we square the random numbers");
 
             foreach (int num in nums2)
+            {
+                Console.WriteLine(num);
+            }
+
+            Console.WriteLine("now we remove the odd numbers");
+
+            //Where() returns a new object linked to nums2 and filters the even numbers
+            //ToList() creates a new list object with the collection and returns the collection
+            var removeOdds = nums2.Where(x => x % 2 == 0).ToList();
+
+            foreach (int num in removeOdds)
             {
                 Console.WriteLine(num);
             }
