ae7234fa3ab7700a6ba9f7140970d51781a0c6ec
Nathan Pabst <nathan.pabst@gmail.com>
2018-09-09 12:24:06 -0500

    random numbers and squared values printing

--- RandomSquared/Program.cs
+++ RandomSquared/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace RandomSquared
 {
@@ -6,7 +7,35 @@ namespace RandomSquared
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Random rando = new Random();
+            List<int> nums = new List<int>();
+            List<int> nums2 = new List<int>();
+            List<int> evenNums = new List<int>();
+
+            for (int i = 0; i <= 20; i++)
+            {
+                var results = rando.Next(1, 51);
+                nums.Add(results);
+            }
+            Console.WriteLine("here are some random numbers");
+
+            foreach (int num in nums)
+            {
+                Console.WriteLine(num);
+                nums2.Add(num * num);
+            }
+
+            Console.WriteLine("now we square the random numbers");
+
+            foreach (int num in nums2)
+            {
+                Console.WriteLine(num);
+            }
+
+            Console.ReadLine();
+
         }
+
     }
+
 }
